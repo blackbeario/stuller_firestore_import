15311d4ee4d8ca18c58e093b9455b2f92ff0cd2a
Jeff Frazier <jeff@black-bear.io>
2019-09-26 22:11:16 -0400

    Create function to search for field type and retrieve data

--- vcardToJson.js
+++ vcardToJson.js
@@ -8,13 +8,9 @@ vcard.parseVcardFile('./test.vcf', function(err, data){
   // Add logic here to setup json fields correctly.
   try {
     data.forEach(element => {
-      // console.log(element);
       // Setup the firstName and lastName from fullname.
       $firstName = element.fullname.substr(0,element.fullname.indexOf(' '));
       $lastName = element.fullname.substr(element.fullname.indexOf(' ')+1);
-
-      // Create the loc/lat for the geopoint.
-      // $position = new firebaseAdmin.firestore.GeoPoint(35.6416958, -82.254216)
 
       // Create the new customer array.
       arr.customers[element.fullname] = {
@@ -24,21 +20,36 @@ vcard.parseVcardFile('./test.vcf', function(err, data){
         'main': element.phone[0].value,
         'notes': element.note,
         'locations': {
-          'primary': { 
+          'primary': {
             'name': 'home',
             'address': element.addr[0].street,
             'city': element.addr[0].city,
             'state': element.addr[0].region,
             'zipcode': element.addr[0].zip,
             'area': element.addr[0].locality,
-            'billing': element.billing,
             'generators': {
               'gen': {
                 'model': element.generator,
-                'serial': element.serial
+                'serial': element.serial,
+                'battery age': element.battery,
+                'xfer location': element.xferLocation,
+                'xfer serial': element.xferSerial,
+                'spark plugs': element.sparkPlugs,
+                'exercise time': element.exercise,
+                'oil filter': element.oilFiler,
+                'air filter': element.airFilter,
+                'warranty': element.warranty,
+                'location': element.location,
+                'wifi': element.wifi
               }
             }
-          }
+          },
+          'billing': {
+            'address': element.billing[0].street,
+            'city': element.billing[0].city,
+            'state': element.billing[0].region,
+            'zipcode': element.billing[0].zip,
+          },
         }
       };
     });
