2206b8fd81b22fdff1c39a9c9c452e7d866ecfb5
Jeff Frazier <jeff@black-bear.io>
2019-10-01 00:13:08 -0400

    Update vcardToJson.js and cloud functions to successfully import ALL of the customers and create geopoints in the cloud

--- functions/index.js
+++ functions/index.js
@@ -9,19 +9,52 @@ const googleMapsClient = require('@google/maps').createClient({
 // Triggers must always point to a document, not a collection.
 exports.createGeoPoint = functions.firestore
   .document('customers/{customerID}/locations/primary')
-  .onUpdate( async (change) => {
-    const afterData = change.after.data();
-    const previousData = change.before.data();
-
-    // We'll only update if the geopoint has changed.
-    // This is crucial to prevent infinite loops.
-    if (afterData.address === previousData.address) return null;
+  .onCreate(async (snap) => {
+    // const document = change.after.exists ? change.after.data() : null;
+    const newData = snap.data();
     var geo = {};
     
     // Use these fields to compute the latitude/longitude.
-    const $address = afterData.address + ', ' + afterData.city + ', ' + afterData.state;
+    const $address = newData.address + ', ' + newData.city + ', ' + newData.state;
 
-    await googleMapsClient.geocode({address: $address}) 
+    await googleMapsClient.geocode({address: $address})
+      .asPromise()
+      .then((response) => {
+        let latitude = response.json.results[0].geometry.location.lat;
+        let longitude = response.json.results[0].geometry.location.lng;
+        geo.lat = latitude;
+        geo.lng = longitude;
+        return geo;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // Then return a promise of a set operation to update the geopoint.
+    return snap.ref.set({
+      updated: new Date(),
+      // Add the computed lat/long to add a geopoint field.
+      geopoint: new admin.firestore.GeoPoint(geo.lat, geo.lng)
+    }, {merge: true});
+  });
+
+
+  // Triggers must always point to a document, not a collection.
+exports.updateGeoPoint = functions.firestore
+.document('customers/{customerID}/locations/primary')
+.onUpdate( async (change) => {
+  const afterData = change.after.data();
+  const previousData = change.before.data();
+
+  // We'll only update if the geopoint has changed.
+  // This is crucial to prevent infinite loops.
+  if (afterData.address === previousData.address) return null;
+  var geo = {};
+  
+  // Use these fields to compute the latitude/longitude.
+  const $address = afterData.address + ', ' + afterData.city + ', ' + afterData.state;
+
+  await googleMapsClient.geocode({address: $address})
     .asPromise()
     .then((response) => {
       let latitude = response.json.results[0].geometry.location.lat;
@@ -34,10 +67,10 @@ exports.createGeoPoint = functions.firestore
       console.log(err);
     });
 
-    // Then return a promise of a set operation to update the geopoint.
-    return change.after.ref.set({
-      updated: new Date(),
-      // Add the computed lat/long to add a geopoint field.
-      geopoint: new admin.firestore.GeoPoint(geo.lat, geo.lng)
-    }, {merge: true});
-  });
+  // Then return a promise of a set operation to update the geopoint.
+  return change.after.ref.set({
+    updated: new Date(),
+    // Add the computed lat/long to add a geopoint field.
+    geopoint: new admin.firestore.GeoPoint(geo.lat, geo.lng)
+  }, {merge: true});
+});
--- vcardToJson.js
+++ vcardToJson.js
@@ -1,63 +1,72 @@
-const firebaseAdmin = require('firebase-admin');
 var vcard = require('vcard-json');
 const fs = require('fs');
-var file = './test.json';
+var file = './StullerContacts.json';
 var arr = {customers:{}};
 
-vcard.parseVcardFile('./test.vcf', function(err, data){
+vcard.parseVcardFile('./StullerContacts.vcf', function(err, data){
   // Add logic here to setup json fields correctly.
   try {
     data.forEach(element => {
       // Setup the firstName and lastName from fullname.
       $firstName = element.fullname.substr(0,element.fullname.indexOf(' '));
       $lastName = element.fullname.substr(element.fullname.indexOf(' ')+1);
+      
+      // If there's a billing address, create a location for it in Firestore.
+      if (element.billing[0]) {
+        billing = {
+          'address': element.billing[0].street,
+          'city': element.billing[0].city,
+          'state': element.billing[0].region,
+          'zipcode': element.billing[0].zip
+        }
+      }
+      // An empty object will not create an address in Firestore.
+      else billing = {};
 
       // Create the new customer array.
       arr.customers[element.fullname] = {
         'firstName': $firstName,
         'lastName': $lastName,
-        'email': element.email[0].value,
-        'main': element.phone[0].value,
+        'email': element.email[0] ? element.email[0].value : '',
+        'main': element.phone[0] ? element.phone[0].value : '',
+        'mobile': element.cell,
         'notes': element.note,
+        'payment': element.payment,
         'locations': {
+          billing,
           'primary': {
             'name': 'home',
-            'address': element.addr[0].street,
-            'city': element.addr[0].city,
-            'state': element.addr[0].region,
-            'zipcode': element.addr[0].zip,
-            'area': element.addr[0].locality,
+            'address': element.addr[0] ? element.addr[0].street : '',
+            'city': element.addr[0] ? element.addr[0].city : '',
+            'state': element.addr[0] ? element.addr[0].region : '',
+            'zipcode': element.addr[0] ? element.addr[0].zip : '',
+            'area': element.addr[0] ? element.addr[0].locality : '',
             'generators': {
               'gen': {
-                'model': element.generator,
+                'generator': element.generator ? element.generator : element.nickname,
+                'model': element.model,
                 'serial': element.serial,
                 'battery age': element.battery,
                 'xfer location': element.xferLocation,
                 'xfer serial': element.xferSerial,
                 'spark plugs': element.sparkPlugs,
                 'exercise time': element.exercise,
-                'oil filter': element.oilFiler,
+                'oil filter': element.oilFilter,
                 'air filter': element.airFilter,
                 'warranty': element.warranty,
                 'location': element.location,
                 'wifi': element.wifi
               }
             }
-          },
-          'billing': {
-            'address': element.billing[0].street,
-            'city': element.billing[0].city,
-            'state': element.billing[0].region,
-            'zipcode': element.billing[0].zip,
-          },
+          }
         }
       };
     });
 
-    // console.log(arr);
-
     // Write the array to a new json file.
     fs.writeFileSync(file, JSON.stringify(arr))
+    $date = new Date();
+    console.log('finished', $date);
   } catch (err) {
     console.error(err)
   }
